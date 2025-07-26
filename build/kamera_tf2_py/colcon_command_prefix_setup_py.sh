c5dd1cc14ac8e3b52e3a804f9a459cd21d57335e
Nikola Akrap <nikola.akrap02@gmail.com>
2025-07-26 16:45:01 +0200

    remove build, install, and log from tracking

--- build/kamera_tf2_py/colcon_command_prefix_setup_py.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/nikolaakrap/zavrsni/install/pymoveit2/share/pymoveit2/package.sh"
--- build/pymoveit2/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nikolaakrap/zavrsni/src/pymoveit2
-# Build directory: /home/nikolaakrap/zavrsni/build/pymoveit2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
